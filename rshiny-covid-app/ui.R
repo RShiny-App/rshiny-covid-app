3535cadcd685ae38bbf677f8d6b4af173b6317b3
alxmtzr <ametzler188@googlemail.com>
2023-12-31 16:34:41 +0100

    added refereces.txt, added www folder for images, created first app layout

--- rshiny-covid-app/ui.R
+++ rshiny-covid-app/ui.R
@@ -9,25 +9,59 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+# set working directory
+setwd("D:/Code/rstudio_workbench/rshiny-covid-app/rshiny-covid-app/")
+
 fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+  # Application title
+  titlePanel(title=tags$div(
+    #img(src="www/logo.svg", 
+    img(src="https://www.thu.de/_catalogs/masterpage/HSUlm/images/logo.svg", 
+    alt="thu_logo", 
+    height=60,
+    align="right"), "Covid App")),
+  
+    # tabset panel with different contents
+    tabsetPanel(
+      # introduction tab
+      tabPanel("Introduction",
+               # content of introduction
+               fluidRow(
+                 column(12,
+                        tags$div(
+                          h3("Einführung zu Covid-19"),
+                          p("Hier sollte eine Einführung zu Covid 19 stehen")
+                        ))
+               )),
+      # graph tab
+      tabPanel("GraphA",
+               # content of graph A
+               fluidRow(
+                 column(12,
+                        tags$div(
+                          h3("Graph A"),
+                          p("Hier steht bald ein Graph.")
+                        ))
+               )),
+      # graph tab
+      tabPanel("GraphB",
+               # content of graph B
+               fluidRow(
+                 column(12,
+                        tags$div(
+                          h3("Graph B"),
+                          p("Hier steht bald ein Graph.")
+                        ))
+               )),
+      # about tab
+      tabPanel("About",
+               fluidRow(
+                 column(12,
+                        tags$div(
+                          h3("Contributors"),
+                          p("Alexander Metzler, Florian Hauptmann, Yannik Krantz")
+                        ))
+               ))
     )
 )
