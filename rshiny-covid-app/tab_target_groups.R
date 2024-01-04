175502b0d482674a9424244d3d26f69e45227c10
alxmtzr <ametzler188@googlemail.com>
2024-01-04 16:57:44 +0100

    first look of target groups and firstdose

---
+++ rshiny-covid-app/tab_target_groups.R
@@ -0,0 +1,49 @@
+library(readr)
+library(tibble)
+library(ggplot2)
+library(plotly)
+
+rm(list = ls())
+
+df_tibble = as_tibble(readr::read_csv("rshiny-covid-app/resources/data.csv"))
+
+View(df_tibble)
+
+names(df_tibble)
+
+refusals_not_zero <- df_tibble %>%
+  filter(FirstDoseRefused != 0)
+
+
+# sum value of first dose per target group
+sum_first_dose <- df_tibble %>%
+  group_by(TargetGroup) %>%
+  summarise(Sum_FirstDose = sum(FirstDose, na.rm = TRUE)) %>%
+  arrange(desc(Sum_FirstDose))
+
+print(sum_first_dose)
+
+
+########################### pie chart selected age groups #######################
+selected_target_groups <- c("Age0_4", "Age5_9", "Age10_14", "Age15_17", 
+                            "Age18_24", "Age25_49", "Age50_59", "Age60_69", 
+                            "Age70_79", "Age80+")
+
+
+# sum value of first dose per target group (selected target groups)
+sum_first_dose <- df_tibble %>%
+  filter(TargetGroup %in% selected_target_groups) %>%
+  group_by(TargetGroup) %>%
+  summarise(Sum_FirstDose = sum(FirstDose, na.rm = TRUE)) %>%
+  arrange(desc(Sum_FirstDose))
+
+# pie chart
+pie_chart <- ggplot(sum_first_dose, aes(x = "", y = Sum_FirstDose, fill = TargetGroup)) +
+  geom_bar(stat = "identity", width = 1, color = "white") +
+  coord_polar("y", start = 0) +
+  theme_void() +
+  labs(title = "Verteilung der FirstDose nach Altersgruppen")
+
+print(pie_chart)
+
+
