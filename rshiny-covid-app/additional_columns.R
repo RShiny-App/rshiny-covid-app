3d61f0d6b4bd3712915b38924f0b52ac6b3fb9f6
YannikKrantz <krantz@mail.hs-ulm.de>
2024-01-05 16:36:03 +0100

    Added new Columns

---
+++ rshiny-covid-app/additional_columns.R
@@ -0,0 +1,30 @@
+#Adding columns
+#
+#
+# Add a new column that sums up doses across all columns
+df$DosesThisWeek <- rowSums(df[c("FirstDose", "SecondDose", "AdditionalDose", "MoreAdditionalDoses", "UnknownDose")])
+
+# Make a new dataframe for all the relevant TargetGroups
+new_df <- df %>%
+  filter(TargetGroup %in% c('ALL', 'Age0_4', 'Age5_9', 'Age10_14', 'Age15_17', 'Age18_24', 'Age25_49', 'Age50_59', 'Age60_69', 'Age70_79', 'Age80+')) %>%
+  select(Year, Week, Region, Vaccine, DosesThisWeek) %>%
+  distinct() %>%
+  group_by(Year, Week, Region) %>%
+  summarise(TotalDosesThisWeek = sum(DosesThisWeek, na.rm = TRUE)) %>%
+  ungroup()
+
+
+# Merge new_df with the original dataframe based on 'YearWeekISO' and 'Region'
+df <- merge(df, new_df, by = c("Year","Week", "Region"), all.x = TRUE)
+
+# Calculate PercentageOfTotalDosesInWeek
+df$PercentageOfTotalDosesInWeek <- (df$DosesThisWeek / df$TotalDosesThisWeek) * 100
+
+# Display the updated dataframe
+View(df)
+
+
+
+
+
+
